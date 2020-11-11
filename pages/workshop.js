c2248b0c5d3a705663c3d0615bf544ee9a86f249
Kat <31995175+KatWBerlin@users.noreply.github.com>
2020-11-11 19:42:32 +0100

    Update workshop.js

--- pages/workshop.js
+++ pages/workshop.js
@@ -8,20 +8,52 @@ export default class extends React.Component {
   render() {
     return (
       <Layout>
-        <h1>Workshop</h1>
+        <h1>Yin Yoga with Reiki (Online)</h1>
         <Contact/>
 
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.
-        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-        looked up one of the more obscure Latin words, consectetur,
-        from a Lorem Ipsum passage, and going through the cites of the word in classical literature,
-        discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of
-        "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-        This book is a treatise on the theory of ethics, very popular during the Renaissance.
-        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
-        comes from a line in section 1.10.32.
-        </p>
+        <p>Let's keep our vibration high and all come together and stay connected during these times! 
+           Judith and I have created what we hope will be a magical experience for everyone! 
+            We look forward to seeing you all! ♥ Love from Berlin ♥</p>
+    
+        <p>Mondays until Christmas: 7pm CET // 1pm EST
+          Thursdays until Christmas: 11am CET // 5AM EST</p>
+          
+          <p>Come and join us weekly for a restoring Yin Yoga and Reiki session. In each session, we will focus on one of the 5 elements of Traditional Chinese Medicine. 
+          The first sessions on November 16th and November 19th we will work with the metal element. The metal element relates to late autumn. Our practice focusing on the 
+          large intestine and lung pathways will strengthen our immune system, respiratory tract, build our self-esteem, helps us form boundaries and connect us to the 
+          present moment. We will start with a guided meditation led by Judith, followed by a yoga practice with Kat and end with an extended final relaxation where Judith 
+          will be sending Kundalini Reiki to everyone. 
+
+          PRACTICAL INFORMATION
+
+          You will need:
+          - One or two blankets
+          - A sturdy pillow (round, square) that you can use as a bolster
+          - Optional: yoga blocks & eye pillow
+
+          We will provide you a Spotify playlist that you can play in the background but you can put on any (soft) music you would like or no music at all.
+
+          Energy exchange: 17,50 Euro (incl. service fees)
+
+          When signing up for the first class on Monday November 16 or Thursday November 19, we invite you to use the following discount codes: YINLOVE80 for 80% off, 
+          or YINLOVE50 for 50% off, or YINLOVE25 for 25% off.
+
+          Sign up here: 
+          Monday: https://www.yugenwithjudith.com/events/yin-yoga-kundalini-reiki-class
+          Thursday: https://www.yugenwithjudith.com/events/yin-yoga-kundalini-reiki-class-8r5a2-42ae7
+
+          We will also open the “Zoom” room 15 minutes early if you want to test if the technical part is working and/or if you just want to hang out and exchange a few 
+          words with the participants. 
+
+          ABOUT THE FACILITATORS
+
+          Kat is a certified Yin Yoga, Thai Yoga Massage and Meridian Yoga Therapy Practitioner. She is a passionate life-long student, teacher and loves to serve the community. 
+          Her lineage believes that by serving others, we heal ourselves.
+
+          Judith is a Kundalini Reiki Master, Intuition Guide, Harpist, and Tea Sommelier. She gives energy and sound healing treatments, teaches Intuitive Development & Kundalini
+          Reiki classes and imports and sells her own direct trade Specialty Tea.</p>
+
+
 
 
       </Layout>
