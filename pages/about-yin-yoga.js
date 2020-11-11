19cee39ffcaebb10e754b23d8659d8d877e659c1
Kat <31995175+KatWBerlin@users.noreply.github.com>
2020-11-11 11:23:38 +0100

    Update about-yin-yoga.js

--- pages/about-yin-yoga.js
+++ pages/about-yin-yoga.js
@@ -7,19 +7,28 @@ export default class extends React.Component {
   render() {
     return (
       <Layout>
-        <h1>About Yin Yoga</h1>
+        <h1><b>About Meridian Yoga Therapy<b></h1>
 
-        <p>Contrary to popular belief, Lorem Ipsum is not simply random text.
-        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-        looked up one of the more obscure Latin words, consectetur,
-        from a Lorem Ipsum passage, and going through the cites of the word in classical literature,
-        discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of
-        "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-        This book is a treatise on the theory of ethics, very popular during the Renaissance.
-        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
-        comes from a line in section 1.10.32.
+        <p><i>Meridian Yoga Therapy combines ancient Chinese and Yogic principles to remove blockages + stagnation that may manifest as symptoms or dis-ease. 
+      Leave with a deeper knowing of your body, foster your healing, balance your energy + release stress and tension.<i>
         </p>
+      
+      <p>Modern lifestyle poses its challenges resulting in emotional and physical patterns that can get us uncomfortable, stuck or hinder us in daily or recreational activities. 
+      Meridian Yoga Therapy combines ancient Chinese and Yogic principles to remove energetic blockages and qi stagnation that manifest as symptoms or dis-ease. 
+      A regular routine focusing on these pathways and points will aid our body to return to a state of balance and health. The practice will take you on a journey to dive deep 
+      into yourself and explore the wonders of your body and the points and pathways running through it, fostering healing, body awareness and energetic balancing. 
+      We will use asana, breathing techniques and acupressure points to ease the flow of energy through these channels and relieve stress, muscle aches, pain, tension, headaches, 
+      digestive issues, anxiety, depression and postural issues.</p>
+      
+      <h2><b>About the meridians<b></h2>
+      
+      <p>Our vital energy (also called Qi or Prana) flows through a network of energetic pathways in our body. Chinese Medicine calls them meridians, the Yogis call them nadis. 
+    Along these pathways lie acupressure points. These pathways and points connect our organs and their functions. Imagine the underground train map of a larger city. 
+    Each train line is a meridian and the stops are acupressure points. If there is an issue at one of the stops, it interferes with the whole line. 
+    The location where two or more lines cross at an intersection forms points of importance. For energy and blood to be evenly distributed within our body, the pathways 
+    need to be unobstructed. By holding certain asana and pressing acupressure points, we can unblock and activate the points and pathways that run through our body, 
+      restoring the natural functions of our tissue and organs. We can clear excess energy or infuse deficient energy.</p>
+Edit
 
       </Layout>
     )
